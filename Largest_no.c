5d3a9b02edd26428e44a58758cf248baf6481748
sam <samish707@gmail.com>
2024-10-05 12:34:44 +0530

    2nd program

---
+++ Largest_no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=30,c=40;
+    if(a>b && a>c)
+    
+        printf("a is largest number");
+    
+    else if(c>a && c>b)
+        
+                  printf("c is greatest");
+            
+    else
+                  printf(" b is greatest");
+                 
+}
